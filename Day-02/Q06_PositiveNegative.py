c8c64393445e004870470b7bc182a7657ad900c4
aryan tripathi <aryantripathi@aryans-MacBook-Air.local>
2026-07-27 10:08:02 +0530

    Added Q06 PositiveNegative

---
+++ Day-02/Q06_PositiveNegative.py
@@ -0,0 +1,16 @@
+# 📝 Question:
+# Write a Python program to check whether a number is:
+
+# Positive
+# Negative
+# Zero
+
+number =  float(input("Enter a number: "))
+ 
+if number > 0:
+     print("The number is Positive.")
+elif number < 0:
+    print("The number is Negative. ")
+else:
+    print("The number is Zero. ")
+         
---
+++ Day-02/Q07_LargestOfTwo.py
@@ -0,0 +1,2 @@
+# Write a Python program to find the largest of two numbers.
+
