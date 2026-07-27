4e858fdf93ee041783e30eeec0e5dc2c4b678e7d
aryan tripathi <aryantripathi@aryans-MacBook-Air.local>
2026-07-27 10:33:02 +0530

    Renamed Q07_LargestOfTwo.py

--- Day-02/Q07_LargestOfTwo.py
+++ Day-02/Q07_LargestOfTwo.py
@@ -1,2 +1,9 @@
 # Write a Python program to find the largest of two numbers.
 
+num1 = int(input())
+num2 = int(input())
+
+if num1 > num2:
+    print(f"{num1} is greater")
+else:
+    print(f"{num2} is greater")    
